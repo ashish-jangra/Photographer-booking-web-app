882b0addc04bb25ff842c136f81f94907d76c7bd
ashish-jangra <ashishjangra3760@gmail.com>
2019-04-12 04:53:02 +0530

    added session authentication

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyparser = require('body-parser');
 const mysql = require('mysql')
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
 const dbconn = mysql.createConnection({
     host:'localhost',
@@ -23,9 +25,39 @@ const app=express();
 app.set('view engine','ejs');
 app.use(express.static('public'));
 app.use(bodyparser.urlencoded({extended:false}));
+app.use(cookieParser());
+app.use(session({
+    key: 'user_sid',
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true
+}));
 
 const server=app.listen(4000,function(){
 	console.log("Listening to request on port 4000");
+});
+
+app.get("/",function(req,res){
+    sql = "select * from users where username = '" + req.session.user.username + "' and password = '" + req.session.user.password + "';";
+    dbconn.query(sql,function(err,result){
+        console.log("/ request",result);
+        if(result){
+            res.redirect("/dashboard");
+        }
+        else{
+            res.sendFile(__dirname+"/index.html");
+        }
+    });
+});
+
+app.get("/dashboard",function(req,res){
+    console.log("dashbard request",req.session.user);
+    if(req.session.user && req.cookies.user_sid){
+        res.render("user",req.session.user);
+    }
+    else{
+        res.redirect("/");
+    }
 });
 
 app.get("/signup_user",function(req,res){
@@ -50,16 +82,18 @@ app.post("/register_user",function(req,res){
                 res.redirect('/');
             }
             else{
+                req.session.user = {username: req.body.username, password: req.body.password};
                 sql = "insert into users values('" + req.body.username + "','" + req.body.name + "','" + req.body.email + "','" + req.body.password + "');";
                 dbconn.query(sql,function(err,result){
                     if(err){
                         throw err;
+                        res.redirect("/signup_user");
                     }
                     else{
-                        console.log(result);
+                        console.log("successfully signed up " + req.body.username);
                     }
                 });
-                res.render('/user',req.body);
+                res.redirect('/dashboard');
             }
         }
     });
@@ -80,37 +114,34 @@ app.post("/register_photographer",function(req,res){
             }
             else{
                 sql = "insert into photographers values('" + req.body.username + "','" + req.body.name + "','" + req.body.email + "','" + req.body.password + "','" + req.body.phone + "','" + req.body.address + "','" + req.body.city + "');";
+                req.session.user = {username: req.body.username, password: req.body.password};
                 dbconn.query(sql,function(err,result){
                     if(err){
                         throw err;
+                        res.redirect("/signup_photographer");
                     }
                     else{
-                        console.log(result);
+                        console.log("registered photographer "+req.body.username);
                     }
                 });
-                res.render('/photographer',req.body);
+                res.redirect("/dashboard");
             }
         }
     });
 });
 
 app.post("/login",function(req,res){
-    let sql = "select * from users where username = '" + req.body.username + "' and password = '" + req.body.password + "';";
+    sql = "select * from users where username = '" + req.body.username + "' and password = '" + req.body.password + "';";
+    console.log("login query: "+sql);
     dbconn.query(sql,function(err,result){
-        if(err){
-            console.log(err);
-            throw err;
-            res.redirect('/');
+        if(result){
+            console.log("logged in");
+            req.session.user = {username: req.body.username, password: req.body.password};
+            res.redirect("/dashboard");        
         }
         else{
-            if(result.length == 0){
-                console.log("wrong username or password");
-                res.redirect('/');
-            }
-            else{
-                console.log("successfully logged in");
-                res.render("user",req.body);
-            }
+            console.log("wrong username or password");
+            res.redirect("/");
         }
     });
 });
@@ -119,6 +150,14 @@ app.get("/contact",function(req,res){
     res.render("contact.ejs",req.query);
 });
 
-app.post("/book_profile",function(req,res){
-    sql = "select * from "
+app.get("/book_photographer",function(req,res){
+    console.log(req.query);
+});
+
+app.get("/logout",function(req,res){
+    if(req.session.user && req.cookies.user_sid){
+        req.session.destroy();
+        req.clearCookie('user_sid');
+    }
+    res.redirect("/");
 });
